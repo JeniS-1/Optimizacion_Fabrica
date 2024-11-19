903b840af8a78c992a1cda75ad471b8e9d1e61d3
JeniS-1 <jen_2002@hotmail.es>
2024-11-19 12:41:18 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,60 +2,49 @@
 #include "funciones.h"
 
 int main() {
-    
-    char productos[NumProduct][30];
-    float tiempos[NumProduct], recursos[NumProduct];
-    int cantidades_demandadas[NumProduct];
-    float tiempos_disponibles[NumProduct], recursos_disponibles[NumProduct];
-    int cantidades_producidas[NumProduct];
-    int num_ingresados = 0, opcion, continuar;
+    char nombres_productos[NumProduct][30] = {""};
+    float tiempos_por_unidad[NumProduct] = {0};
+    int num_recursos[NumProduct] = {0};
+    float recursos[NumProduct][MaxResources] = {{0}};
+    int cantidad_demandada[NumProduct] = {0};
+    int cantidad_producida[NumProduct] = {0};
+    int num_ingresados = 0;
+    char nom_producto[NumProduct][20];
 
+    int opcion;
     do {
-        
-        for (int i = 0; i < NumProduct; i++) {
-            productos[i][0] = '\0';  
-            tiempos[i] = 0;
-            recursos[i] = 0;
-            cantidades_demandadas[i] = 0;
-            tiempos_disponibles[i] = 0;
-            recursos_disponibles[i] = 0;
-            cantidades_producidas[i] = 0;
-        }
-        num_ingresados = 0;
-
-        printf("\nIngreso de Productos\n");
-        for (int i = 0; i < NumProduct; i++) {
-            printf("\nProducto %d de %d:\n", i + 1, NumProduct);
-            ingresarProducto(productos[i], &tiempos[i], &recursos[i], &cantidades_demandadas[i],&tiempos_disponibles[i], &recursos_disponibles[i]);
-            evaluarProduccionIndividual(productos[i], tiempos[i], recursos[i], cantidades_demandadas[i], tiempos_disponibles[i], recursos_disponibles[i], &cantidades_producidas[i]);
-            num_ingresados++;
-        }
-
-        do {
-            opcion = menu();
-            switch (opcion) {
-                case 1:
-                    mostrarInventario(productos, tiempos, recursos, cantidades_producidas, num_ingresados);
-                    break;
-                case 2:
-                    buscarProducto(productos, tiempos, recursos, cantidades_demandadas, tiempos_disponibles, recursos_disponibles, cantidades_producidas, &num_ingresados);
-                    break;
-                case 3: {
-                    printf("\n¿Desea ingresar 5 productos nuevos? (1: Si, 0: No): ");
-                    int reiniciar;
-                    scanf("%d", &reiniciar);
-                    getchar();
-                    if (reiniciar) {
-                        opcion = -1; 
-                    } else {
-                        printf("\nSaliendo del programa definitivamente.\n");
-                        return 0;
-                    }
-                    break;
+        opcion = menu();
+        switch (opcion) {
+            case 1:
+                ingresarProducto(nombres_productos, tiempos_por_unidad, num_recursos, recursos, num_ingresados, nom_producto);
+                if (num_ingresados < NumProduct) {
+                    num_ingresados++;
                 }
-                default:
-                    printf("\nOpcion invalida.\n");
-            }
-        } while (opcion != -1); 
+                break;
+            case 2:
+                mostrarInventario(nombres_productos, tiempos_por_unidad, num_recursos, recursos, cantidad_demandada, cantidad_producida, num_ingresados);
+                break;
+            case 3:
+                buscarProducto(nombres_productos, tiempos_por_unidad, num_recursos, recursos, cantidad_demandada, cantidad_producida, &num_ingresados);
+                break;
+            case 4:
+                hacerPedido(nombres_productos, tiempos_por_unidad, num_recursos, recursos, cantidad_demandada, cantidad_producida, num_ingresados);
+                break;
+            case 5:
+                printf("\n¿Desea ingresar más productos? (1: Sí, 0: No): ");
+                int continuar;
+                scanf("%d", &continuar);
+                getchar();
+                if (!continuar) {
+                    printf("\nSaliendo del programa.\n");
+                    return 0;
+                }
+                break;
+            default:
+                printf("\nOpción inválida.\n");
+        }
     } while (1);
+
+    return 0;
 }
+
