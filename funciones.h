77b769211afbe568f73c6da169eb966f75f5c2cd
JeniS-1 <jen_2002@hotmail.es>
2024-11-19 12:41:42 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,8 +1,10 @@
+
 #define NumProduct 5
+#define MaxResources 3
 
 int menu();
-void ingresarProducto(char *producto, float *tiempo, float *recurso, int *cantidad_demandada,float *tiempo_disponible, float *recurso_disponible);
-void evaluarProduccionIndividual(const char *producto, float tiempo, float recurso, int cantidad_demandada,float tiempo_disponible, float recurso_disponible, int *cantidad_producida);
-void mostrarInventario(char productos[NumProduct][30], float *tiempos, float *recursos,int *cantidades_producidas, int num_ingresados);
-void buscarProducto(char productos[NumProduct][30], float *tiempos, float *recursos,int *cantidades_demandadas, float *tiempos_disponibles,float *recursos_disponibles, int *cantidades_producidas, int *num_ingresados);
+void ingresarProducto(char nombres_productos[NumProduct][30], float tiempos_por_unidad[NumProduct], int num_recursos[NumProduct], float recursos[NumProduct][MaxResources], int num_ingresados, char nom_recurso[NumProduct][20]);
+void mostrarInventario(char nombres_productos[NumProduct][30], float tiempos_por_unidad[NumProduct], int num_recursos[NumProduct], float recursos[NumProduct][MaxResources], int cantidad_demandada[NumProduct], int cantidad_producida[NumProduct], int num_ingresados);
+void buscarProducto(char nombres_productos[NumProduct][30], float tiempos_por_unidad[NumProduct], int num_recursos[NumProduct], float recursos[NumProduct][MaxResources], int cantidad_demandada[NumProduct], int cantidad_producida[NumProduct], int *num_ingresados);
+void hacerPedido(char nombres_productos[NumProduct][30], float tiempos_por_unidad[NumProduct], int num_recursos[NumProduct], float recursos[NumProduct][MaxResources], int cantidad_demandada[NumProduct], int cantidad_producida[NumProduct], int num_ingresados);
 
